53af1fc877c47c5bbcd3561a227a97801f189dfd
Pasindu-Lakmal <pasindu1lak@gmail.com>
2023-02-16 13:40:39 +0530

    first

---
+++ src/app/first-graph/first-graph.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import * as mxgraph from 'mxgraph';
+@Component({
+  selector: 'app-first-graph',
+  templateUrl: './first-graph.component.html',
+  styleUrls: ['./first-graph.component.css'],
+})
+export class FirstGraphComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
